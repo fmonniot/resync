0c044e6333b41eff40f30334fd62cbbd01be41d3
François Monniot <francoismonniot@gmail.com>
2021-01-03 10:35:44 -0800

    Generate a cover page as well
    Show story name & chapter for single-chapter, and
    chapter range for multi-chapter

--- app/src/main/java/eu/monniot/resync/Epub.kt
+++ app/src/main/java/eu/monniot/resync/Epub.kt
@@ -1,6 +1,7 @@
 package eu.monniot.resync
 
 import eu.monniot.resync.ui.Chapter
+import eu.monniot.resync.ui.ChapterNum
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import nl.siegmann.epublib.domain.Author
@@ -15,10 +16,11 @@ suspend fun makeEpub(chapterList: List<Chapter>): ByteArray {
     // Create new Book
     val book = Book()
 
-    // When generating a multi chapter, add the chapter name before the start of the chapter
+    // When generating a multi chapter story, add the chapter name before the start of the chapter
     val insertChapterName = chapterList.size > 1
 
-    val chapters = chapterList.sortedBy { it.num }.iterator()
+    val sortedChapters = chapterList.sortedBy { it.num }
+    val chapters = sortedChapters.iterator()
 
     // First chapter is important: it defines the metadata for the entire story
     val firstChapter = chapters.next()
@@ -26,6 +28,10 @@ suspend fun makeEpub(chapterList: List<Chapter>): ByteArray {
     // Story metadata
     book.metadata.addAuthor(Author(firstChapter.author))
     book.metadata.addTitle(firstChapter.storyName)
+    book.addCover(
+        firstChapter,
+        sortedChapters.first().num to sortedChapters.last().num
+    )
 
     // Insert the first chapter
     book.addChapter(firstChapter, insertChapterName)
@@ -62,5 +68,31 @@ ${chapter.content.replace("xmlns=\"http://www.w3.org/1999/xhtml\"", "")}
 
     val resource = Resource(content.toByteArray(), "chapter_${chapter.num}.xhtml")
 
-    this.addSection(chapter.chapterName ?: "", resource);
+    this.addSection(chapter.chapterName ?: "", resource)
+}
+
+// Note that remarkable will ignore the layout when internally converting from epub to pdf.
+// Emitting as a PDF would make us loose the typo/size choice on the tablet.
+// So we keep the layout for now and hope that the reader will be able to decode those in the future
+// (even though I highly doubt that will be the case).
+fun Book.addCover(chapter: Chapter, range: Pair<ChapterNum, ChapterNum>) {
+    val (first, last) = range
+
+    val subTitle = when {
+        first == last && chapter.chapterName != null -> chapter.chapterName
+        first == last -> ""
+        else -> "Chapters $first to $last"
+    }
+
+    val content = """<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
+<body style="margin: 10% 15%;padding:0;">
+<h1 style="margin-top: 25%;width:100%;">${chapter.storyName}</h1>
+<h2 style="margin-top: 5%;">$subTitle</h2>
+<hr style="margin:0 10% 0 10%"></hr>
+<h2>By ${chapter.author}</h2>
+</body>
+</html>"""
+
+    this.coverPage = Resource(content.toByteArray(), "cover.xhtml")
 }
